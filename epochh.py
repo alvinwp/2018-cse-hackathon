deafc7c80c13284d09b2c6c62117ceebd9d4f78f
Jerry Zhu <jerryzhu96@hotmail.com>
2018-09-22 17:15:11 +1000

    added epoch

--- epochh.py
+++ epochh.py
@@ -1,37 +1,49 @@
 import time
 import datetime
-
-
-timee = time.time()
-print(timee)
-
+import re
 
 # time given as seconds from epoch
 def get_day(time):
     time = datetime.datetime.fromtimestamp(time)
     print(time.weekday()+1)
 
-date_time = '30.08.2011 11:05:02'
-pattern = '%d.%m.%Y %H:%M:%S'
-epoch = int(time.mktime(time.strptime(date_time, pattern)))
-print(epoch)
-
-get_day(epoch)
-
-
 #
 # current time object
 
-def week_difference(time, date_string):
-    d1 = datetime.datetime.fromtimestamp(time)
+# def week_difference(day, month, year):
+#     now = datetime.date.today() 
+#     dt = datetime.datetime.strptime(str(year)+'-'+str(month)+'-'+str(day), '%Y-%m-%d')
+#     print(now - dt)
 
-    date_time = '30.08.2011 11:05:02'
-    pattern = '%d.%m.%Y %H:%M:%S'
-    epoch = int(time.mktime(time.strptime(date_time, pattern)))
+def getWeekInt():
+    # current YEAR WEEK is 38
+    # current UNI WEEK is 9
+    # 38-9 = 29
+    WEEK_BUFFER = 29
+    
+    curr_week = int(time.strftime("%W", time.localtime(time.time())))
+    
+    # MID SEM WEEK is YEAR WEEK 39
+    # if midsem break return -1
+    if curr_week == 39:
+        return -1
+    # so add 1 week buffer after midsem break
+    elif curr_week >= 40:
+        WEEK_BUFFER = WEEK_BUFFER + 1
+        
+    return curr_week - WEEK_BUFFER        
 
-    d2 = datetime.datetime.fromtimestamp(epoch)
-    result = (d1-d2).days//7
+def getTimeInt():
+    curr_time = time.strftime("%H:%M", time.localtime(time.time()))
+    
+    s = re.search('([0-9]+):([0-9]+)', curr_time)
+    
+    if int(s.group(2)) <= 30:
+        int_time = int(s.group(1))*2
+    else:
+        int_time = int(s.group(1))*2 + 1
 
+    return int_time
 
 # start = 23-Jul-2018
 # mid = 22-Sep-2018
@@ -43,13 +55,10 @@ def week_difference(time, date_string):
 
 
 
-# #!/usr/bin/python
-# import time
-
-t = (2009, 2, 17, 17, 3, 38, 1, 48, 0)
-t = time.mktime(t)
-print(time.strftime("%b %d %Y %H:%M:%S", time.localtime(t)))
+# t = (2009, 2, 17, 17, 3, 38, 1, 48, 0)
+# t = time.mktime(t)
+# print(time.strftime("%b %d %Y %H:%M:%S", time.localtime(t)))
 # time.localtime
 
 # converts time to string
-# month a
+# month 
